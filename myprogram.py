ea2e2f086b278179c08ce0f3fda7740df33e3668
VishalDhalani <70875874+VishalDhalani@users.noreply.github.com>
2021-01-07 13:12:31 +0530

    Add files via upload

---
+++ myprogram.py
@@ -0,0 +1,8 @@
+from mymodule import mymod
+from MainPackage import main_script
+from MainPackage.SubPackage import sub_script
+
+
+mymod()
+main_script.main_check()
+sub_script.sub_check()
---
+++ simple1.py
@@ -0,0 +1,7 @@
+"""
+A simple program to check via pylint
+"""
+A = 1
+B = 2
+print(A)
+print(B)
